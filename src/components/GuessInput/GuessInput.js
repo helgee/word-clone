213f38231846c9efbc7879be04dc252a48ada2c9
Helge Eichhorn <git@helgeeichhorn.de>
2023-06-22 13:31:08 +0200

    Finish ex1

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    console.log({ guess });
+    setGuess("");
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        pattern="[a-zA-Z]{5,5}"
+        onChange={(evt) => setGuess(evt.target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
